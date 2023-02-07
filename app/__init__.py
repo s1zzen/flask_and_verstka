db8246a5a49491fe5fc16404d95afd31b27e0106
s1zzen <dbaskaktyuzxc@gmail.com>
2023-02-07 19:44:18 +0300

    del assets

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from config import Config
 
-from flask_assets import Environment, Bundle
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -11,9 +9,5 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-assets = Environment(app)
-scss = Bundle('main.scss', filters='pyscss', output='assets/all.css')
-assets.register('scss_all', scss)
 
 from app import routes, models, forms
